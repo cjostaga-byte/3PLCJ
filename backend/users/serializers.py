785b046f85bca35f9011b360440a196169983e16
cjostaga-byte <cjostaga@gmail.com>
2026-02-18 23:22:02 +0800

    Adding DRF serializers for authentication and attendance.

---
+++ backend/users/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+class AttendanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attendance
+        fields = ['id', 'user', 'check_in', 'check_out']
